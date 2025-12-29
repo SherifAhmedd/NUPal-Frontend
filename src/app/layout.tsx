3b984996fa49210bfb233077d967ac1ef82d0477
abdelrahman-a99 <a.ahmed2299>
2025-12-30 01:59:25 +0200

    Add suppressHydrationWarning to the body tag in the layout.tsc to handle the hydration errors from the google extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="flex min-h-screen flex-col">
+      <body className="flex min-h-screen flex-col" suppressHydrationWarning>
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
